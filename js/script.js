a79913d439f00d454e7ca28ba8f80f22da01bb92
zhytus <136345750+zhytus@users.noreply.github.com>
2023-06-20 22:46:12 +0700

    reverse Button

--- js/script.js
+++ js/script.js
@@ -1,30 +1,80 @@
-function convert() {
-  var celsiusInput = document.getElementById("celsius");
+//Used To Convert Celcius To Fahrenheit
+function convertCelcius() {
+  var celciusInput = document.getElementById("input");
   var result = document.getElementById("result");
 
   // Used to detect if the input is null or a string type
-  if (celsiusInput.value === "" || isNaN(celsiusInput.value)) {
-    alert("Please enter a valid temperature in Celsius Value");
+  if (celciusInput.value === "" || isNaN(celciusInput.value)) {
+    alert("Please enter a valid temperature in Celcius Value");
     resetFields();
     return;
   } else {
-    // Convert Celsius to Fahrenheit
-    var celsius = parseFloat(celsiusInput.value);
-    var fahrenheit = (celsius * 9) / 5 + 32;
+    // Convert Celcius to Fahrenheit
+    var celcius = parseFloat(celciusInput.value);
+    var fahrenheit = (celcius * 9) / 5 + 32;
 
     // Display the result, and the logic
-    result.textContent = "Result : " + celsius + "°C = " + fahrenheit + "°F";
+    result.textContent = "Result : " + celcius + "°C = " + fahrenheit + "°F";
     logic.textContent =
-      "How To Get " + fahrenheit + "°F : " + celsius + "°C * (9/5) + 32 ";
+      "How To Get " + fahrenheit + "°F : " + celcius + "°C * (9/5) + 32 ";
   }
 }
 
-// function use to clear celcius, result, and logic Value
-function resetFields() {
-  var celsiusInput = document.getElementById("celsius");
-  var result = document.getElementById("result");
+function reverseCelcius() {
+  // Reverse To Celcius
+  location.reload();
+}
 
-  celsiusInput.value = "";
+//Used to change from "Celcius To Fahrenheit" to "Fahrenheit To Celcius"
+function reverseFahrenheit() {
+  resetFields();
+  var fahrenheitInput = document.getElementById("input");
+  var label = document.querySelector('label[for="input"]');
+  var convertButton = document.querySelector(
+    'button[onclick="convertCelcius()"]'
+  );
+  var reverseButton = document.querySelector(
+    'button[onclick="reverseFahrenheit()"]'
+  );
+
+  // Change Label and Input Placeholder to Fahrenheit
+  label.textContent = "Fahrenheit:";
+  fahrenheitInput.placeholder = "Enter temperature in Fahrenheit";
+
+  // Change Event "onClick" for Convert and Reverse Button
+  convertButton.setAttribute("onclick", "convertFahrenheit()");
+  reverseButton.setAttribute("onclick", "reverseCelcius()");
+}
+
+//Used To Convert Fahrenheit To Celcius
+function convertFahrenheit() {
+  var fahrenheitInput = document.getElementById("input");
+  var result = document.getElementById("result");
+  var logic = document.getElementById("logic");
+
+  // Check if Fahrenheit input is empty or not a number
+  if (fahrenheitInput.value === "" || isNaN(fahrenheitInput.value)) {
+    alert("Please enter a valid temperature in Fahrenheit.");
+    return;
+  }
+
+  // Convert Fahrenheit to Celcius
+  var fahrenheit = parseFloat(fahrenheitInput.value);
+  var celcius = ((fahrenheit - 32) * 5) / 9;
+
+  // Display the result
+  result.textContent = "Result: " + fahrenheit + "°F = " + celcius + "°C";
+  logic.textContent =
+    "How To Get " + celcius + "°C: (" + fahrenheit + "°F - 32) * 5/9";
+}
+
+// function use to clear input, result, and logic Value
+function resetFields() {
+  var Input = document.getElementById("input");
+  var result = document.getElementById("result");
+  var logic = document.getElementById("logic");
+
+  Input.value = "";
   result.textContent = "";
   logic.textContent = "";
 }
